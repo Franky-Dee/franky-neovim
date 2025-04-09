37d0c957d05bd6e071b372ffa2f724d900ec3f09
franco <franco@taskflow.co.za>
2025-04-09 17:00:33 +0200

    [REM] lsp-config: Removed unused

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
         'williamboman/mason-lspconfig.nvim',
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "ruff", "eslint", "cssls" }
+                ensure_installed = { "lua_ls" }
             })
         end
     },
@@ -19,17 +19,7 @@ return {
             local capabilities = require('cmp_nvim_lsp').default_capabilities()
             local lspconfig = require("lspconfig")
 
-            -- Add more lsp's here in the same format as lua_ls
             lspconfig.lua_ls.setup({
-                capabilities = capabilities
-            })
-            lspconfig.ruff.setup({
-                capabilities = capabilities
-            })
-            lspconfig.eslint.setup({
-                capabilities = capabilities
-            })
-            lspconfig.cssls.setup({
                 capabilities = capabilities
             })
 
