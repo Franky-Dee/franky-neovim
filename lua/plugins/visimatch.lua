be22a532366951c59daca50c0b1a0c9c9973dd43
franco <franco@taskflow.co.za>
2025-05-27 19:17:59 +0200

    [ADD] visimatch.lua: Added highlighting other identical words in visual mode

---
+++ lua/plugins/visimatch.lua
@@ -0,0 +1,11 @@
+return {
+    "wurli/visimatch.nvim",
+    opts = {
+        hl_group = "Search",
+        chars_lower_limit = 6,
+        lines_upper_limit = 30,
+        strict_spacing = false,
+        buffers = "filetype",
+        case_insensitive = { "markdown", "text", "help" },
+    }
+}
