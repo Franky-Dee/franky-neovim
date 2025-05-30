e511c10c975ea7deb6913dba68987c121402e8e0
franco <franco@taskflow.co.za>
2025-05-30 09:38:11 +0200

    [ADD] treejs: Added so searches can be made inline or below each other

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -39,3 +39,6 @@ vim.keymap.set('n', '<A-h>', '<C-w>h', {})
 vim.keymap.set('n', '<A-j>', '<C-w>j', {})
 vim.keymap.set('n', '<A-k>', '<C-w>k', {})
 vim.keymap.set('n', '<A-l>', '<C-w>l', {})
+
+-- Remove Search
+vim.keymap.set('n', '<Leader>n', ':noh<CR>', {})
