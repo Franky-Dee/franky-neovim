a8f7b0310a8051a7f31dea2974bfdea34b660b4d
franco <franco@taskflow.co.za>
2025-06-04 08:50:55 +0200

    [IMP] lsp-config: Added config for pylsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,31 +1,59 @@
 return {
     {
+        -- Mason: Installs LSP servers
         "williamboman/mason.nvim",
         config = function()
             require("mason").setup()
         end
     },
     {
-        'williamboman/mason-lspconfig.nvim',
+        -- Bridge between mason and lspconfig
+        "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls" }
+                ensure_installed = { "lua_ls" } -- Add other LSP servers as needed
             })
         end
     },
     {
-        'neovim/nvim-lspconfig',
+        -- LSP config
+        "neovim/nvim-lspconfig",
         config = function()
-            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
             local lspconfig = require("lspconfig")
 
+            -- Set up lua language server
             lspconfig.lua_ls.setup({
                 capabilities = capabilities
             })
 
-            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-            vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+            lspconfig.pylsp.setup({
+                capabilities = capabilities,
+                settings = {
+                    pylsp = {
+                        plugins = {
+                            pycodestyle = {
+                                enabled = true,
+                                ignore = { "E501" }, -- E501 = line too long
+                                maxLineLength = 120, -- optional: increase line limit if you still want long lines allowed
+                            },
+                        },
+                    },
+                },
+            })
+
+            -- LSP key mappings
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+
+            -- Configure diagnostics display
+            vim.diagnostic.config({
+                virtual_text = true,      -- Inline text (e.g. "undefined variable")
+                signs = true,             -- Show signs in the gutter
+                underline = true,         -- Underline the problem text
+                update_in_insert = false, -- Don't update diagnostics in insert mode
+                severity_sort = true,     -- Sort by severity
+            })
         end
     }
 }
